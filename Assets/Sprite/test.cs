9bfb196781ce5231081e279b4bfd53518f44525b
kusuby <hsub1202@hanyang.ac.kr>
2025-06-30 00:04:38 +0900

    update

--- Assets/Sprite/test.cs
+++ Assets/Sprite/test.cs
@@ -1,6 +1,10 @@
+using System.Runtime.InteropServices.WindowsRuntime;
+using Unity.VisualScripting.FullSerializer;
 using UnityEngine;
 
 public class test
 {
+    int n = 1;
     
+    ReturnValueNameAttribute 0;
 }
