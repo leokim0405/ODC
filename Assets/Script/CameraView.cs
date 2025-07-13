6ac3197fea1e137ebedc76cdca06ac115bcc65c7
minseok <leokim0405@naver.com>
2025-07-13 23:12:31 +0900

    floor, stone initial

---
+++ Assets/Script/CameraView.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+public class CameraView : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        Vector3 position = new Vector3(0, 80, 0);
+        transform.position = position;
+
+        Vector3 lookTarget = new Vector3(position.x, 0, position.z);
+        transform.LookAt(lookTarget);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
